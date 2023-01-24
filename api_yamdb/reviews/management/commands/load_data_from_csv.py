3a21f9390a1bd078c33a6849a95b780fc2c5ded7
andrew_cherry <chernyaevav@gmail.com>
2023-01-24 21:33:11 +0300

    исправление load_data_from_csv.py

--- api_yamdb/reviews/management/commands/load_data_from_csv.py
+++ api_yamdb/reviews/management/commands/load_data_from_csv.py
@@ -1,155 +1,128 @@
-from csv import DictReader
 
-from django.core.management import BaseCommand
+import csv
+import os
 
-from reviews.models import Category, Comment, Genre, Review, Title
+import django.db.utils
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand
+
+from reviews.models import Comment, Review, Category, Genre, Title
 from users.models import User
 
-ALREDY_LOADED_ERROR_MESSAGE = """
-Перед загрузкой данных из CSV-файла, удалите файл БД db.sqlite3.
-Создайте миграции `python manage.py makemigrations`.
-Выполните миграции `python manage.py migrate`.
-"""
+DATA = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv'
+}
+
+
+def read_csv(name_file):
+    """Считывает данные из csv и возвращает список строк таблицы"""
+    path = os.path.join('static/data', name_file)
+    with open(path, encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        return list(reader)
+
+
+def get_list_fields_model(model):
+    """
+    Принимает объект модели, и возвращает словарь с полями в виде:
+    {<поле модели>: <поле в таблице>}
+    """
+    fields_obj_list = model._meta.fields
+    fields = {
+        field.name: field.attname for field in fields_obj_list
+    }
+    return fields
+
+
+def changes_fields(fields_model, table):
+    """
+    Изменяет название полей прочитанной таблицы
+    для корректной записи в БД
+    """
+    for row in table:
+        for name_field in list(row):
+            if (
+                    name_field in fields_model
+                    and name_field
+                    != fields_model[name_field.replace("_id", "")]
+            ):
+                row[fields_model[name_field]] = row.pop(name_field)
+
+
+def load_data(model, name_file):
+    """
+    Загрузка данных по имени модели.
+    Не загружает данные во вспомогательную таблицу
+    со связью многие ко многим
+    """
+    table = read_csv(name_file)
+    changes_fields(get_list_fields_model(model), table)
+    model.objects.bulk_create(model(**row) for row in table)
+
+
+def load_genre_title():
+    """
+    Загружает данные во вспомогательную таблицу
+    для моделей со связью многие ко многим
+    """
+    data_list = read_csv('genre_title.csv')
+    [
+        Title.objects.get(id=row['title_id']).genre.add(row['genre_id'])
+        for row in data_list
+    ]
+
+
+def del_data():
+    """Удаляет все таблицы из базы данных"""
+    for model in DATA:
+        model.objects.all().delete()
 
 
 class Command(BaseCommand):
-    help = "Загрузка данных из CSV файлов"
+    help = 'Импортирует таблицы из csv в базу данных'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-a',
+            '--all',
+            action='store_true',
+            help='Импортирует все таблицы из csv в базу данных'
+        )
+        parser.add_argument(
+            '-c',
+            '--clear',
+            action='store_true',
+            help='Удаляет все данные из базы данных'
+        )
 
     def handle(self, *args, **options):
-
-        if (
-            Category.objects.exists()
-            or Genre.objects.exists()
-            or Title.objects.exists()
-            or Review.objects.exists()
-            or Comment.objects.exists()
-            # or User.objects.exists()
-        ):
-            print('в таблице Жанры уже содержатся данные.')
-            print(ALREDY_LOADED_ERROR_MESSAGE)
-            return
-
-        file_path = 'static/data/users.csv'
-        data_list = []
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f'Загрузка данных из {file_path}... ', ending='')
-            for row in reader:
-                data_list.append(
-                    User(
-                        id=row['id'],
-                        username=row['username'],
-                        email=row['email'],
-                        role=row['role'],
-                        bio=row['bio'],
-                        first_name=row['first_name'],
-                        last_name=row['last_name'],
-                    )
-                )
-            User.objects.bulk_create(data_list)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-        file_path = 'static/data/category.csv'
-        data_list = []
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f'Загрузка данных из {file_path}... ', ending='')
-            for row in reader:
-                data_list.append(Category(name=row['name'], slug=row['slug']))
-            Category.objects.bulk_create(data_list)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-        file_path = 'static/data/genre.csv'
-        data_list = []
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f'Загрузка данных из {file_path}... ', ending='')
-            for row in reader:
-                data_list.append(Genre(name=row['name'], slug=row['slug']))
-            Genre.objects.bulk_create(data_list)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-        file_path = 'static/data/titles.csv'
-        data_list = []
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f'Загрузка данных из {file_path}... ', ending='')
-            for row in reader:
-                data_list.append(
-                    Title(
-                        name=row['name'],
-                        year=row['year'],
-                        category_id=row['category'],
-                    )
-                )
-            Title.objects.bulk_create(data_list)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-        file_path = 'static/data/review.csv'
-        data_list = []
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f"Загрузка данных из {file_path}... ", ending='')
-            for row in reader:
-                data_list.append(
-                    Review(
-                        title_id=row['title_id'],
-                        text=row['text'],
-                        author_id=row['author'],
-                        score=row['score'],
-                        pub_date=row['pub_date'],
-                    )
-                )
-            Review.objects.bulk_create(data_list)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-        file_path = 'static/data/comments.csv'
-        data_list = []
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f"Загрузка данных из {file_path}... ", ending='')
-            for row in reader:
-                data_list.append(
-                    Comment(
-                        review_id=row['review_id'],
-                        text=row['text'],
-                        author_id=row['author'],
-                        pub_date=row['pub_date'],
-                    )
-                )
-            Comment.objects.bulk_create(data_list)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-        file_path = 'static/data/genre_title.csv'
-        with open(file_path, mode="r", encoding="utf-8") as file:
-            reader = DictReader(file)
-            self.stdout.write(f"Загрузка данных из {file_path}... ", ending='')
-            for row in reader:
-                title_obj = Title.objects.get(id=row['title_id'])
-                genre = Genre.objects.get(id=row['genre_id'])
-
-                title_obj.genre.add(genre)
-        self.stdout.write(self.style.SUCCESS("OK"))
-
-# ============================================================================
-#         file_path_list = {
-#             User: 'static/data/users.csv',
-#             Category: 'static/data/category.csv',
-#             Genre: 'static/data/genre.csv',
-#             Title: 'static/data/titles.csv',
-#             Review: 'static/data/review.csv',
-#             Comment: 'static/data/comment.csv',
-#         }
-#
-#         for model, file_path in file_path_list.items():
-#             with open(
-#                 f'E:/ДОкументы/my_Python/api_yamdb/api_yamdb/{file_path}',
-#                 mode="r", encoding="utf-8"
-#             ) as file:
-#                 reader = DictReader(file)
-#                 self.stdout.write(
-#                     f"Загрузка данных из {file_path}... ",
-#                     ending=''
-#                 )
-#                 model.objects.bulk_create(model(**data) for data in reader)
-#             self.stdout.write(self.style.SUCCESS("OK"))
+        try:
+            if options['all']:
+                for model, name_file in DATA.items():
+                    load_data(model, name_file)
+                load_genre_title()
+                self.stdout.write(
+                    self.style.SUCCESS('Таблицы загружены в базу данных.'))
+            elif options['clear']:
+                del_data()
+                self.stdout.write(
+                    self.style.SUCCESS('База данных успешно очищена.'))
+            else:
+                self.stdout.write(
+                    self.style.SQL_KEYWORD('Команда используется с ключом,'
+                                           ' список всех ключей: --help'))
+        except django.db.utils.IntegrityError as e:
+            self.stdout.write(
+                self.style.ERROR('Ошибка загрузки. База данных не пуста. '
+                                 'Совпадение уникальных полей. "%s"' % e))
+        except ObjectDoesNotExist:
+            self.stdout.write(
+                self.style.NOTICE('Нет данных из связанных таблиц'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR('Ошибка загрузки данных:'
+                                               ' "%s"' % e))
